306b36dac110853d518cd2dc325b502b59bb233b
Zhuofeng Wang <zhuofengwang2003@gmail.com>
2025-02-24 15:05:52 +0800

    tidy up

--- lua/luasnip-latex-snippets/init.lua
+++ lua/luasnip-latex-snippets/init.lua
@@ -1,2 +1,2 @@
-snippet_path = debug.getinfo(1).source:sub(2):gsub("init.lua", "luasnippets")
+local snippet_path = debug.getinfo(1).source:sub(2):gsub("init.lua", "luasnippets")
 require("luasnip.loaders.from_lua").lazy_load({ paths = snippet_path })
